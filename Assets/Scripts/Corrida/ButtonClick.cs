89aa1b01334699608007c3b9154e6f911e41ad3a
Vitor <vitor.e.demedeiros@gmail.com>
2023-08-03 21:57:28 -0300

    Criação do minigame 3

--- Assets/Scripts/Corrida/ButtonClick.cs
+++ Assets/Scripts/Corrida/ButtonClick.cs
@@ -8,15 +8,21 @@ public class ButtonClick : MonoBehaviour
     [SerializeField] private Button buttonclick;
     [SerializeField] private GameObject quadrado;
     private float speed = 16.25f;
+    private int clicado = 0;
 
     private void Awake()
     {
-        buttonclick.onClick.AddListener(cliked);
+        buttonclick.onClick.AddListener(Cliked);
     }
 
-    private void cliked()
+    private void Cliked()
     {
         quadrado.transform.position = new Vector2(quadrado.transform.position.x + speed, quadrado.transform.position.y);
+        clicado++;
+        if (clicado == 8)
+        {
+            Debug.Log("Você venceu!");
+        }
     }
 }
 
