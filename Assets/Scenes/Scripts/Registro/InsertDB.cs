8c8e99c4bbaa691d05974e7c1be9df7f4a8fb5f0
Vitor <113442437+Mr-Vitor@users.noreply.github.com>
2024-01-07 19:02:11 -0300

    Commit #sei lá

--- Assets/Scenes/Scripts/Registro/InsertDB.cs
+++ Assets/Scenes/Scripts/Registro/InsertDB.cs
@@ -1,36 +1,78 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Mono.Data.Sqlite;
-using Mono.Data;
 using System.Data;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
 
 public class InsertDB : MonoBehaviour
 {
-    public string dbName;
-    public void Inserir()
+    public InputField NameInput;
+    public InputField SobrenomeInput;
+    public InputField EmailInput;
+    public InputField PasswordInput;
+    public string DataBaseName;
+    public Text CadastroStatus;
+
+    public void InsertInto()
     {
-        string conn = SetDataBaseClass.SetDataBase(dbName + ".db");
+        var _NameInput = NameInput.text.Trim();
+        var _SobrenomeInput = SobrenomeInput.text.Trim();
+        var _EmailInput = EmailInput.text.Trim();
+        var _PasswordInput = PasswordInput.text.Trim();
+
+        if (string.IsNullOrEmpty(_NameInput) || string.IsNullOrEmpty(_SobrenomeInput) ||
+            string.IsNullOrEmpty(_EmailInput) || string.IsNullOrEmpty(_PasswordInput))
+        {
+            CadastroStatus.text = "Todos os campos devem ser preenchidos.";
+            return;
+        }
+
+        if (_EmailInput.Length < 7)
+        {
+            CadastroStatus.text = "E-mail inválido. O email deve ter pelo menos 7 caracteres.";
+            return;
+        }
+
+        if (_PasswordInput.Length < 8)
+        {
+            CadastroStatus.text = "A senha deve ter pelo menos 8 caracteres.";
+            return;
+        }
+
+        string conn = SetDataBaseClass.SetDataBase(DataBaseName + ".db");
         IDbConnection dbcon;
         IDbCommand dbcmd;
-        IDataReader reader;
 
         dbcon = new SqliteConnection(conn);
         dbcon.Open();
         dbcmd = dbcon.CreateCommand();
-        string SqlQuery = "Enter your query";
-        dbcmd.CommandText = SqlQuery;
-        reader = dbcmd.ExecuteReader();
-        while (reader.Read())
-        {
+        string SQlQuery = "Insert Into Usuarios(nome, sobrenome, email, senha)" +
+                          "Values('" + _NameInput + "', '" + _SobrenomeInput + "', '" + _EmailInput + "', '" + _PasswordInput + "')";
+        dbcmd.CommandText = SQlQuery;
 
+        try
+        {
+            dbcmd.ExecuteNonQuery();
+            Debug.Log("Inserção bem-sucedida.");
+            LoadScenes("Login");
         }
-        reader.Close();
-        reader = null;
+        catch (System.Exception)
+        {
+            CadastroStatus.text = "E-mail já cadastrado";
+        }
+
         dbcmd.Dispose();
         dbcmd = null;
         dbcon.Close();
         dbcon = null;
+    }
+
+    public void LoadScenes(string cena)
+    {
+        SceneManager.LoadScene(cena);
     }
 
 
